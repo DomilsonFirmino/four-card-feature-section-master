783e2241958a5e2948f5eb36b4ed9f1ca00cd3c7
DomilsonFirmino <almeidagilbertofirmino@gmail.com>
2023-07-16 12:42:19 +0200

    colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        red: 'hsl(0, 78%, 62%)',
+        cyan: 'hsl(180, 62%, 55%)',
+        orange: 'hsl(34, 97%, 64%)',
+        blue: 'hsl(212, 86%, 64%)',
+        darkBlue: 'hsl(234, 12%, 34%)',
+        grayishBlue: 'hsl(229, 6%, 66%)',
+        lightGray: 'hsl(0, 0%, 98%)'
+      }
+    },
   },
   plugins: [],
 }
